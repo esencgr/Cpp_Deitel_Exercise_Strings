74eafcc8d82a964b73979254cfa4e503a773ab15
Cagri Esen <esencgr@gmail.com>
2019-07-15 14:17:54 +0300

    modified

--- string_conversion_functions.cpp
+++ string_conversion_functions.cpp
@@ -22,12 +22,18 @@ int main(){
     cout << "long int value  : " << x << endl;
     cout << "extra string    : " << extra_ptr1 << endl << endl;
 
-    const char *str2 = "-1234567acbf";
+    const char *str2 = "1234567acbf";
     char * extra_ptr2;
     long y = strtol( str2, &extra_ptr2, 0 );
     cout << "original string : " << str2 << endl;
     cout << "long int value  : " << y << endl;
-    cout << "extra string    : " << extra_ptr2 << endl;
+    cout << "extra string    : " << extra_ptr2 << endl << endl;
 
+    const char *str3 = "-1234567acbf";
+    char * extra_ptr3;
+    unsigned long z = strtoul( str3, &extra_ptr3, 0 );
+    cout << "original string : " << str3 << endl;
+    cout << "long int value  : " << z << endl;
+    cout << "extra string    : " << extra_ptr3 << endl;
     return 0;
 }
